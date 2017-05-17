41722449cc3c3e250bda7f1c21f118d7aed704c5
John Bulauan <JBulauan@johns-air.gateway>
2017-05-17 19:37:50 +1000

    saving customer data and comments

--- RaceMyCar/RaceMyCar/AppDelegate.swift
+++ RaceMyCar/RaceMyCar/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -40,6 +41,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+
 
 
 }
--- RaceMyCar/RaceMyCar/ViewBooking.swift
+++ RaceMyCar/RaceMyCar/ViewBooking.swift
@@ -11,6 +11,23 @@ import MapKit
 
 class ViewBooking: UIViewController, MKMapViewDelegate {
 
+    
+    @IBOutlet weak var FirstName: UITextField!
+    @IBOutlet weak var LastName: UITextField!
+    @IBOutlet weak var Dob: UITextField!
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Address: UITextField!
+    @IBOutlet weak var Suburb: UITextField!
+    @IBOutlet weak var State: UITextField!
+    @IBOutlet weak var PostCode: UITextField!
+    @IBOutlet weak var NameOnCard: UITextField!
+    @IBOutlet weak var CardNumber: UITextField!
+    @IBOutlet weak var Month: UITextField!
+    @IBOutlet weak var Year: UITextField!
+    @IBOutlet weak var SecurityCode: UITextField!
+    
+    @IBOutlet weak var BookingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,16 +39,102 @@ class ViewBooking: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    //Once buttons are pressed, the next segue will commence
-    @IBAction func PaymentInfo(_ sender: UIButton) {
-        performSegue(withIdentifier:"CustomerInfo", sender: sender.tag)
-        
-    }
     
     //Reverts back to previous segue. Back button will be linked to this code
     @IBAction func unWindSegue(segue: UIStoryboardSegue) {
     }
 
+    //Save customer information and payment once entered. We will use userdefaults to make this work.
+    @IBAction func SaveInfo(_ sender: UIButton) {
+        UserDefaults.standard.set(FirstName.text, forKey: "firstname")
+        UserDefaults.standard.set(LastName.text, forKey: "lastname")
+        UserDefaults.standard.set(Dob.text, forKey: "dob")
+        UserDefaults.standard.set(Email.text, forKey: "email")
+        UserDefaults.standard.set(Address.text, forKey: "address")
+        UserDefaults.standard.set(Suburb.text, forKey: "suburb")
+        UserDefaults.standard.set(State.text, forKey: "state")
+        UserDefaults.standard.set(PostCode.text, forKey: "postcode")
+        UserDefaults.standard.set(NameOnCard.text, forKey: "nameoncard")
+        UserDefaults.standard.set(CardNumber.text, forKey: "cardnumber")
+        UserDefaults.standard.set(Month.text, forKey: "month")
+        UserDefaults.standard.set(Year.text, forKey: "year")
+        UserDefaults.standard.set(SecurityCode.text, forKey: "securitycode")
+        
+    }
+    
+    //ensuring that saving the customer information will not crash.
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if let i = UserDefaults.standard.object(forKey: "firstname") as? String
+        {
+            FirstName.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "lastname") as? String
+        {
+            LastName.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "dob") as? String
+        {
+            Dob.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "email") as? String
+        {
+            Email.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "address") as? String
+        {
+            Address.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "suburb") as? String
+        {
+            Suburb.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "state") as? String
+        {
+            State.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "postcode") as? String
+        {
+            PostCode.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "nameoncard") as? String
+        {
+            NameOnCard.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "cardnumber") as? String
+        {
+            CardNumber.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "month") as? String
+        {
+            Month.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "year") as? String
+        {
+            Year.text = i
+        }
+        
+        if let i = UserDefaults.standard.object(forKey: "securitycode") as? String
+        {
+            SecurityCode.text = i
+        }
+        
+    }
+    
+   
+    
+     
     
     
 
